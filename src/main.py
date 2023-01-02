50318327390679f6961297e497ed9e7b4682066c
Or <orelias.tm@gmail.com>
2023-01-02 22:46:10 +0200

    Added support for pagination in graphql, changed function's names

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 
 
-
 class RepositoryVulnerablityAlertQuery(object):
 	createdAtField = "createdAt"
 	securityVulnerabilityField = " securityVulnerability { severity }  "
@@ -13,31 +12,61 @@ class RepositoryVulnerablityAlertQuery(object):
 
 
 class Dependabot():
-	def __init__(self, auth_token, graphql_endpoint):
+	def __init__(self, auth_token, graphql_endpoint, print_debug_log=False):
+		self.debug = print_debug_log
 		transport = RequestsHTTPTransport( url=graphql_endpoint, headers = {"Authorization": f"bearer {auth_token}"} )
 		self.client = Client(transport=transport)
+		print(f"Successfully created a graphql client") if self.debug is True else None
 
 
-	def get_vulnerablties_of_repository(self,
-										repository_owner,
-										repository_name,
-										fields=[RepositoryVulnerablityAlertQuery.securityVulnerabilityField,
-											    RepositoryVulnerablityAlertQuery.createdAtField]):
+
+	def get_security_alerts(self, repository_owner: str, repository_name: str, fields=[RepositoryVulnerablityAlertQuery.securityVulnerabilityField, RepositoryVulnerablityAlertQuery.createdAtField]):
+		# for more information please read the official docs - https://github.com/or-elias/pyDependabot
+		#
+		# Input parameters:
+		#	- repository_owner - sometimes refered to as the organization of the repository. i.e. microsoft
+		#   - repository_name - the full name of the repo. i.e. vscode
+		#   - fields - not mandatory. a list of consts from RepositoryVulnerablityAlertQuery class. represents the data we ask from the server.
+		# 
+		# Output - 
+		#	for now the function returns a list of raw nodes as returned from the server. 
+		#	also, if we received a graphql error from github (for example, NOT FOUND), we let the gql.transport.exceptions.TransportQueryError excption to be raised.
+		#
+
+
+		return self._query_api_for_repositories_vulnerablities(repository_owner, repository_name, fields)
 		
-		query = gql(self.build_query_to_fetch_vulnerablities(repository_owner, repository_name, fields))
-		results = self.client.execute(query)
-		print(results)
 		
 
 
 
 
-	def build_query_to_fetch_vulnerablities(self, repository_owner, repository_name, fields):
-		return f"""
-					{{ 
-						repository( name: "{repository_name}", owner: "{repository_owner}") {{
-						 																		vulnerabilityAlerts(first: 10) {{  nodes{{ {','.join(fields)} }} }}
-																							}} 
-					}}
-				"""
+
+	def _query_api_for_repositories_vulnerablities(self, repository_owner, repository_name, fields, after=None, current_results=[]) -> list[dict]:
+		# A recursive function that returns a list of raw nodes representing vulnerablities alerts from github.
+		# A node is a github graphql object as shown here https://docs.github.com/en/graphql/reference/interfaces#node. we fetch it's RepositoryVulnerability fields.
+		# Note: This function doesnt have recursive depth limitation since we figured a single repository will probably wont have thousands of alerts.
+		#
+
+		q = f"""
+			query GetRepositoryDependabotAlerts ($name: String!, $owner: String!, $after: String) {{ 
+					repository( name: $name, owner: $owner) {{
+																 
+																vulnerabilityAlerts(first: 99, after: $after) {{  
+																									pageInfo {{ hasNextPage, endCursor }} ,
+																									nodes{{ {','.join(fields)} }} }}
+															}} 	
+			}}
+		"""
+		vars = {"name": repository_name, "owner":repository_owner, "after": after}
+		results = self.client.execute(gql(q), variable_values=vars)
+		
+		# Because of the nature of Graphql, I can be sure that I will always get 'endCursor' and 'node' fields in the response, even if they are empty.
+		end_cursor = results.get('repository').get('vulnerabilityAlerts').get('pageInfo').get('endCursor')
+		nodes = results.get('repository').get('vulnerabilityAlerts').get('nodes')
+		
+		if end_cursor is None:
+			return current_results+nodes
+		else:
+			return self._query_api_for_repositories_vulnerablities(repository_owner, repository_name, fields, after=end_cursor, current_results=current_results+nodes)
 			
